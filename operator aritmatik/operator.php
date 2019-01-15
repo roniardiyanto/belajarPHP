50b4cdb9c9887456e9167f34473c6eb3e7c9997c
roniardiyanto <roni.ardiyanto@koding.sch.id>
2019-01-15 10:08:43 +0700

    Operasi aritmatika dasar

---
+++ operator aritmatik/operator.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $angka1 = 34;
+    $angka2 = 15.6;
+    //tambah
+    echo $angka1 + $angka2 . "<br>" ;
+    //kurang
+    echo $angka1 - $angka2 . "<br>" ;
+    //kali
+    echo $angka1 * $angka2 . "<br>";
+    //bagi
+    echo $angka1 / $angka2 . "<br>";
+    ?>
+</body>
+</html>
