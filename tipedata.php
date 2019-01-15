ce758d6da16760cafe0013ce8764c4df616549bd
roniardiyanto <roni.ardiyanto@koding.sch.id>
2019-01-15 09:44:30 +0700

    menambah tipe data float dan integer

--- tipedata.php
+++ tipedata.php
@@ -5,5 +5,12 @@
     echo $string;
     echo $string2;
     echo $string . $string2;
-
+    //tipe data numerik
+        //integer
+            $angka = 12345;
+        //float
+            $angka2 = 12.345;
+    echo $angka;
+    echo $anglka2;
+    echo $angka + $angka2;
 ?>
