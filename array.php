1b0393b7fc70876e591247adfb4c90e821f02e76
Roni Ardiyanto <roni.ardiyanto@koding.sch.id>
2019-01-17 19:40:06 +0700

    revisi belajar array

---
+++ array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //dua cara  menyimpan array
+    $hewan = array('gajah', 'elang', 'kerbau');
+    $bunga = ['mawar', 'melati', 'anggrek'];
+    //pemanggilan array
+    echo $hewan[2] . "<br>";
+    echo $bunga[0] . "<br>";
+    ?>
+</body>
+</html>
