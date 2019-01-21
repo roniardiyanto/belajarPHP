9750156b73df3d470728910683e76bc964420a97
Roni Ardiyanto <roni.ardiyanto@koding.sch.id>
2019-01-21 11:28:50 +0700

    mwnambahkan fungsi else if sebagai syarat kedua jika syarat pertama pada if tidak terpenuhi]

--- nestedIf.php
+++ nestedIf.php
@@ -16,7 +16,7 @@
         if ($uangRoni > $kaosBola) {
             echo 'Roni ada uang untuk beli kaos bola <br>';
             if ($uangRoni >= 3 * $kaosBola) {
-                echo 'Roni akhirnya membeli 3 kaos bola';
+                echo 'Roni akhirnya membeli 3 kaos bola <br>';
             }else{
                 echo 'Roni nggak beli kaos bola';
             }
@@ -24,6 +24,19 @@
         }else{
                 echo 'tidak bisa beli';
         }
+
+        //menggunakan else if
+        if ($utangRoni > $uangRoni) {
+            echo 'Roni masih kurang utangnya <br>';
+        } else if ($utangRoni < $uangRoni){
+            echo 'Roni bisa bayar utang <br>';
+            if ($uangRoni - $utangRoni > $kuotaInternet) {
+                echo 'Roni Sehabis bayar utang , beli kuota internet <br>';
+            }
+        }else {
+            echo 'Roni kemana <br>';
+        }
+        
     ?>
 </body>
 </html>
